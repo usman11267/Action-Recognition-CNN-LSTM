618558ddc8875a2d945a4ca29c6facbb8957891a
usman11267 <muhammad.usman11267@gmail.com>
2026-01-03 23:14:27 +0500

    Update_files

--- backend/app.py
+++ backend/app.py
@@ -193,4 +193,4 @@ if __name__ == '__main__':
     print(f"Endpoint: http://127.0.0.1:5000/predict")
     print("="*50 + "\n")
     
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
